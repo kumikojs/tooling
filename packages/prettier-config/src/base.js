cb89bf3bf3ef3c9c7edfafb6724043cfaa873441
Loïk Fekkaï <lokicoule@yahoo.fr>
2024-11-25 12:33:36 +0100

    feat(prettier-config): add Prettier configuration package

---
+++ packages/prettier-config/src/base.js
@@ -0,0 +1,14 @@
+/** @type {import('prettier').Config} */
+module.exports = {
+  semi: false,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: "none",
+  printWidth: 80,
+  bracketSpacing: true,
+  bracketSameLine: false,
+  arrowParens: "always",
+  endOfLine: "lf",
+  singleAttributePerLine: true,
+  plugins: [],
+};
